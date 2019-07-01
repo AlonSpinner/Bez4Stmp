449a6ca504741dd598a0b20a66ba4656a5742c56
Alon <alonspinner@gmail.com>
2019-07-01 23:45:38 +0300

    Added hausdorff distance+test
    still needs work. something wrong with measurements

--- RunScript.m
+++ RunScript.m
@@ -2,3 +2,8 @@ Stmp=Bez4Stmp('Roie.stl');
 Ax=Stmp.DrawPointCloud(Stmp.PointCloud);
 Stmp.DrawPointCloud(Stmp.CompactControlPoints,'Ax',Ax,'color',[0,0,1],'msize',20);
 Stmp.DrawPointCloud(Stmp.BlownControlPoints,'Ax',Ax,'color',[1,1,1],'msize',20);
+
+a=reshape(Stmp.BlownControlPoints.Location,[],3);
+b=Stmp.PointCloud.Location;
+[hd,aind,bind]=Stmp.Hausdorff(a,b);
+Stmp.DrawPointCloud([a(aind,:);b(bind,:)],'Ax',Ax,'color',[1,0,0],'msize',30);
